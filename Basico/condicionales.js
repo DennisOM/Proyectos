49a7f83ab5f5a9444b33ab82a75c795d40da0263
DennisOM <dennuom@gmail.com>
2026-02-09 19:43:57 -0600

    CopiaClase

---
+++ Basico/condicionales.js
@@ -0,0 +1,39 @@
+//If
+function si(){
+    let fair = 23;
+    let fairCopia = "23";
+    const nair = 15;
+    const smash = 100;
+    const special = 27;
+
+
+
+    if(fair <= fairCopia && smash >= special){
+        console.log("ostion");
+    }else{
+        console.log("Stock cancel");
+    }
+}
+si();
+
+// FOR
+function para(){
+    const fair = 100;
+
+    for(let i = 0; fair > i ; i++){
+
+        console.log("Fair:", fair, "i:", i+1);
+
+    }
+
+}
+
+para();
+
+// WHILE
+function mientras(){
+
+    while(i < fair){
+
+    }
+}
